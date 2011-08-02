edfc2ff897d98ed43bd0dd4043d984136a683add
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-02 12:05:01 -0500

    generate vector tests as closures, so they are each counted individually by lunatest

--- src/test/vector.lua
+++ src/test/vector.lua
@@ -1,44 +1,11 @@
 require("lunatest")
 require("luahapi")
 
-function vector_verification(constructor, dimension)
-	local args1 = {}
-	local args2 = {}
-	for i=1,dimension do
-		table.insert(args1, 1)
-		table.insert(args2, 2)
-	end
 
-	local u = constructor()
-	local v = constructor(unpack(args1))
-	local q = constructor(unpack(args2))
+for dimension=2,4 do
+	local name = "Vec" .. tostring(dimension)
+	local constructor = HAPI[name]
 
-	assert_equal(constructor().dimension, dimension)
-	assert_equal(constructor(unpack(args1)).dimension, dimension)
-	assert_equal(constructor(unpack(args2)).dimension, dimension)
-
-
-	assert_not_equal(constructor(), constructor(unpack(args1)), "Default initialized vector and initialized vector shouldn't match")
-	assert_not_equal(constructor(unpack(args1)), constructor(unpack(args2)), "differently-initialized vectors shouldn't match")
-
-	assert_equal(u, constructor())
-	assert_equal(constructor(), constructor())
-	assert_equal(constructor(unpack(args1)), constructor(unpack(args1)), "identically-initialized vectors should match")
-	assert_equal(constructor(unpack(args2)), constructor(unpack(args2)), "identically-initialized vectors should match")
-
-	assert_equal(v, constructor(v), "copy-constructed vectors should match")
-
-	assert_string(tostring(v))
-	assert_string(tostring(v + q))
-	assert_equal(v + q, q + v)
-
-	assert_equal((v + q) - q, v)
-
-	assert_number(v * q)
-	assert_equal(v * 1, v)
-end
-
-function vector_verification_with_length(constructor, dimension)
 	local args1 = {}
 	local args2 = {}
 	local lenSq1 = 0
@@ -50,25 +17,61 @@ function vector_verification_with_length(constructor, dimension)
 		lenSq2 = lenSq2 + 2 * 2
 		table.insert(args2, 2)
 	end
-	assert_equal(constructor().length, 0)
-	assert_equal(constructor().lengthSqr, 0)
 
-	assert_equal(constructor(unpack(args1)).lengthSqr, lenSq1)
-	assert_equal(constructor(unpack(args2)).lengthSqr, lenSq2)
-	vector_verification(constructor, dimension)
+	local u = constructor()
+	local v = constructor(unpack(args1))
+	local q = constructor(unpack(args2))
+
+	_G[name .. "_dimension_test"] = function()
+		assert_equal(constructor().dimension, dimension)
+		assert_equal(constructor(unpack(args1)).dimension, dimension)
+		assert_equal(constructor(unpack(args2)).dimension, dimension)
+	end
+
+	_G[name .. "_simple_equality_test"] = function()
+		assert_not_equal(constructor(), constructor(unpack(args1)), "Default initialized vector and initialized vector shouldn't match")
+		assert_not_equal(constructor(unpack(args1)), constructor(unpack(args2)), "differently-initialized vectors shouldn't match")
+
+		assert_equal(u, constructor())
+		assert_equal(constructor(), constructor())
+		assert_equal(constructor(unpack(args1)), constructor(unpack(args1)), "identically-initialized vectors should match")
+		assert_equal(constructor(unpack(args2)), constructor(unpack(args2)), "identically-initialized vectors should match")
+	end
+
+	_G[name .. "_copy_constructor_test"] = function()
+		assert_equal(v, constructor(v), "copy-constructed vectors should match")
+		local copy = constructor(q)
+		assert_equal(q, copy)
+		copy.x = -1
+		assert_not_equal(q, copy)
+	end
+
+	_G[name .. "_tostring_test"] = function()
+		assert_string(tostring(v))
+	end
+
+	_G[name .. "_plusminus_test"] = function()
+		assert_equal(v + q, q + v)
+		assert_equal((v + q) - q, v)
+	end
+
+	_G[name .. "_dotproduct_test"] = function()
+		assert_number(v * q)
+	end
+
+	_G[name .. "_scale_test"] = function()
+		assert_equal(v * 1, v)
+	end
+
+	if dimension < 4 then
+		_G[name .. "_length_test"] = function()
+			assert_equal(constructor().length, 0)
+			assert_equal(constructor().lengthSqr, 0)
+
+			assert_equal(constructor(unpack(args1)).lengthSqr, lenSq1)
+			assert_equal(constructor(unpack(args2)).lengthSqr, lenSq2)
+		end
+	end
 end
-
-function test_vec2()
-	vector_verification_with_length(HAPI.Vec2, 2)
-end
-
-function test_vec3()
-	vector_verification_with_length(HAPI.Vec3, 3)
-end
-
-function test_vec4()
-	vector_verification(HAPI.Vec4, 4)
-end
-
 
 lunatest.run()
