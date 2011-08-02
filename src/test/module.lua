d0db9b38f77c6b3c9114dccffaaeb7d6bb612ace
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-02 13:05:22 -0500

    print constructible types in test

--- src/test/module.lua
+++ src/test/module.lua
@@ -3,6 +3,10 @@ require("lunatest")
 function test_loads()
 	require("luahapi")
 	assert_table(HAPI)
+	print("Constructible Types:")
+	for k, v in pairs(HAPI) do
+		print("HAPI." .. k)
+	end
 end
 
 lunatest.run()
