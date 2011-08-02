de32b5c490661bf96e52bda5f9221d7654cd3dce
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-02 13:14:45 -0500

    more impl for HAPIHapticsDevice

--- src/HAPIHapticsDevice.cpp
+++ src/HAPIHapticsDevice.cpp
@@ -32,12 +32,36 @@ template<> luabind::scope bind<HAPI::HAPIHapticsDevice>() {
 	return
 	    class_<HAPI::HAPIHapticsDevice>("HAPIHapticsDevice")
 	    .def("initDevice", &HAPI::HAPIHapticsDevice::initDevice)
+	    .def("initDevice", (void (HAPI::HAPIHapticsDevice::*)())(&HAPI::HAPIHapticsDevice::initDevice))
+
 	    .def("enableDevice", &HAPI::HAPIHapticsDevice::enableDevice)
+
 	    .def("disableDevice", &HAPI::HAPIHapticsDevice::disableDevice)
+
 	    .def("releaseDevice", &HAPI::HAPIHapticsDevice::releaseDevice)
+
 	    .def("setHapticsRenderer", &HAPI::HAPIHapticsDevice::setHapticsRenderer)
-	    //.def("setHapticsRenderer", tag_function<void (HAPI::HAPIHapticsDevice *, HAPI::HAPIHapticsRenderer *)>(boost::bind(&HAPI::HAPIHapticsDevice::setHapticsRenderer, _1, _2)))
 	    .def("setHapticsRenderer", (void (HAPI::HAPIHapticsDevice::*)(HAPI::HAPIHapticsRenderer *))(&HAPI::HAPIHapticsDevice::setHapticsRenderer))
+
+
+	    .def("addShape", &HAPI::HAPIHapticsDevice::addShape)
+	    .def("addShape", (void (HAPI::HAPIHapticsDevice::*)(HAPI::HAPIHapticShape *))(&HAPI::HAPIHapticsDevice::addShape))
+
+	    .def("removeShape", &HAPI::HAPIHapticsDevice::removeShape)
+	    .def("removeShape", (void (HAPI::HAPIHapticsDevice::*)(HAPI::HAPIHapticShape *))(&HAPI::HAPIHapticsDevice::removeShape))
+
+	    .def("clearShapes", &HAPI::HAPIHapticsDevice::clearShapes)
+	    .def("clearShapes", (void (HAPI::HAPIHapticsDevice::*)())(&HAPI::HAPIHapticsDevice::clearShapes))
+
+
+	    .def("addEffect", &HAPI::HAPIHapticsDevice::addEffect)
+	    .def("addEffect", (void (HAPI::HAPIHapticsDevice::*)(HAPI::HAPIForceEffect *))(&HAPI::HAPIHapticsDevice::addEffect))
+
+	    .def("removeEffect", &HAPI::HAPIHapticsDevice::removeEffect)
+	    .def("removeEffect", (void (HAPI::HAPIHapticsDevice::*)(HAPI::HAPIForceEffect *))(&HAPI::HAPIHapticsDevice::removeEffect))
+
+	    .def("clearEffects", &HAPI::HAPIHapticsDevice::clearEffects)
+	    .def("clearEffects", (void (HAPI::HAPIHapticsDevice::*)())(&HAPI::HAPIHapticsDevice::clearEffects))
 	    ,
 	    bind<HAPI::AnyHapticsDevice>()
 	    ;
